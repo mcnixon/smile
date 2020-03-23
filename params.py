ad4ab16ebefa7d75c3a50abbe1efd8b8aa86bcdd
Matthew Nixon <mn442@cam.ac.uk>
2020-03-23 10:08:00 +0000

    latest adaptive attempt

--- params.py
+++ params.py
@@ -4,7 +4,7 @@ import numpy as np
 
 #MR curve settings (MEarth)
 mass_upper = 10
-mass_lower = 0.5
+mass_lower = 5.0
 mass_step = 0.5
 
 #mass fractions: Fe, MgPv, H2O, remainder in H/He
@@ -32,7 +32,16 @@ G = 6.67e-11
 
 eos_files = {}
 
-eos_files['fe'] = '/data/mn442/sunrise/shrek/eos/fe_eos.dat'
-eos_files['mgpv'] = '/data/mn442/sunrise/shrek/eos/mgpv_eos.dat'
-eos_files['h2o'] = '/data/mn442/sunrise/shrek/eos/h2o_eos.hdf5'
-eos_files['hhe'] = '/data/mn442/sunrise/shrek/eos/hhe_eos.hdf5'
+#loads local files if using Mac OS
+import sys
+if sys.platform == 'darwin':
+    eos_files['fe'] = '../eos_data/fe_eos.dat'
+    eos_files['mgpv'] = '../eos_data/mgpv_eos.dat'
+    eos_files['h2o'] = '../eos_data/h2o_eos.hdf5'
+    eos_files['hhe'] = '../eos_data/hhe_eos.hdf5'
+
+else:
+    eos_files['fe'] = '/data/mn442/sunrise/shrek/eos/fe_eos.dat'
+    eos_files['mgpv'] = '/data/mn442/sunrise/shrek/eos/mgpv_eos.dat'
+    eos_files['h2o'] = '/data/mn442/sunrise/shrek/eos/h2o_eos.hdf5'
+    eos_files['hhe'] = '/data/mn442/sunrise/shrek/eos/hhe_eos.hdf5'
--- shrek.py
+++ shrek.py
@@ -14,3 +14,12 @@ def mr_curve():
         radii[i] = int_model.find_Rp()
 
     return np.c_[masses,radii]
+
+def steps_test():
+    int_model = model.Model(0.5)
+    steps = np.logspace(2,5,20)
+    radii = np.zeros_like(steps)
+    for i in range(len(steps)):
+        radii[i] = int_model.find_Rp(steps=int(steps[i]))
+
+    return radii
