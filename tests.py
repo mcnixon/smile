71f6f80ae7f218945623d38d589eaff8eb14c003
mcnixon <mn442@cam.ac.uk>
2024-02-23 13:11:50 -0500

    updated test text outputs

--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ print('Running Earth-like test case:')
 # 1/3 iron, 2/3 silicates
 r_earthlike = smile.get_radius(1.0,P0=1e4,T0=300.0,Pad=1e6,x_w=0.0,x_g=0.0)
 
-print('Radius is '+str(r_earthlike)+' Earth radii (should be around 0.97)')
+print('Radius is '+str(round(r_earthlike,2))+' Earth radii (should be 0.97)')
 
 print('Running sub-Neptune test case (unmixed envelope):')
 
@@ -16,7 +16,7 @@ print('Running sub-Neptune test case (unmixed envelope):')
 # 5% differentiated envelope, half H2O, half H/He, Earth-like nucleus
 r_subnep = smile.get_radius(10.0,P0=1e4,T0=500.0,Pad=1e6,x_w=0.025,x_g=0.025,profiles=False)
 
-print('Radius is '+str(r_subnep)+' Earth radii (should be around 3)')
+print('Radius is '+str(round(r_subnep,2))+' Earth radii (should be 3.0)')
 
 print('Running sub-Neptune test case (mixed envelope):')
 
@@ -24,4 +24,4 @@ print('Running sub-Neptune test case (mixed envelope):')
 # 5% differentiated envelope, half H2O, half H/He, Earth-like nucleus
 r_subnep = smile.get_radius(10.0,P0=1e4,T0=500.0,Pad=1e6,x_w=0.025,x_g=0.025,mixed=True,profiles=False)
 
-print('Radius is '+str(r_subnep)+' Earth radii (should be around 2.57)')
+print('Radius is '+str(round(r_subnep,2))+' Earth radii (should be 2.57)')
